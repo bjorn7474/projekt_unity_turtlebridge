0262a8115c5102796e70dc50ba25686c7303621b
Björn Hallström <student7474@hotmail.com>
2018-09-05 23:46:34 +0200

    problem med asset textmesh pro

---
+++ Assets/Scripts/CarrierController.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrierController : MonoBehaviour {
+
+
+
+    public List<Transform> positions = new List<Transform>();
+    public List<GameObject> turtles = new List<GameObject>();
+    public List<Transform> diePositions2 = new List<Transform>();
+    public List<Transform> diePositions3 = new List<Transform>();
+    public List<Transform> diePositions4 = new List<Transform>();
+    public List<Transform> diePositions5 = new List<Transform>();
+
+    private const int N_TURTLES = 4;
+    private const int N_POS_BENEATH_SURFACE = 4;
+
+    public Texture[] myTextures = new Texture[1];
+
+    int beneathSurfIdx;
+
+    int currentPosition = 0;
+
+    int frameCnt;
+    int mod = 10;
+
+    private bool isDying, dead;
+    private int divingTurtlePosition = -1; // positionen för sköldpaddan som dyker på den platsen där bäraren befinner sig
+
+    public PointsController pointsController;
+  
+
+    private void OnEnable()
+    {
+        Input.OnLeftPressed += Input_OnLeftPressed;
+        Input.OnRightPressed += Input_OnRightPressed;
+    }
+
+    private void OnDisable()
+    {
+        Input.OnLeftPressed -= Input_OnLeftPressed;
+        Input.OnRightPressed -= Input_OnRightPressed;
+    }
+
+    private void Start()
+    {
+
+        
+
+        transform.position = positions[currentPosition].position;
+
+        Renderer m_ObjectRenderer = GetComponent<Renderer>();
+        m_ObjectRenderer.material.mainTexture = myTextures[0];
+
+      
+
+    }
+
+
+
+
+    void Input_OnLeftPressed()
+    {
+        if (currentPosition > 0)
+        {
+            currentPosition--;
+            transform.position = positions[currentPosition].position;
+
+            Debug.Log("position = " + currentPosition);
+
+            //int idx = turtles[0].GetComponent<TurtleController>().getBeneathSurfaceIdx;
+        }
+
+        //surfaceChecker();
+    }
+
+    void Input_OnRightPressed()
+    {
+        if (currentPosition < positions.Count - 1)
+        {
+            currentPosition++;
+            transform.position = positions[currentPosition].position;
+
+        }
+
+        //surfaceChecker();
+    }
+
+
+    void surfaceChecker()
+    {
+
+
+        // utför endast och endast OM bäraren inte håller på att dö - då är ju divingTurtlePosition redan satt och det är den information vi behöver för att "dränka" honom.
+        if (!isDying)
+        {
+            int[] posTurtle = new int[N_TURTLES];
+            for (int i = 0; i < N_TURTLES; i++)
+            {
+                posTurtle[i] = turtles[i].GetComponentInChildren<TurtleController>().getBeneathSurfaceIdx();
+            }
+
+
+            for (int i = 0; i < N_TURTLES; i++)
+            {
+
+                if (currentPosition == i + 1 && posTurtle[N_TURTLES - 1 - i] != 0)
+                {
+                    divingTurtlePosition = i;
+                    isDying = true;
+                }
+            }
+        } 
+
+    }
+
+    private void Update()
+    {
+
+        surfaceChecker();
+
+        if (divingTurtlePosition != -1)
+        {
+
+
+            if (frameCnt % mod == 0)
+            {
+                beneathSurfIdx++;
+            }
+
+            if (beneathSurfIdx > N_POS_BENEATH_SURFACE - 1)
+            {
+                beneathSurfIdx = 0;
+                Debug.Log("!!!!!!!!!!!!!!!!!!!");
+                dead = true;
+
+            }
+
+        
+            switch (divingTurtlePosition)
+            {
+
+
+                case 0:
+                    transform.position = diePositions2[beneathSurfIdx].position;
+                   
+                  
+                    break;
+                case 1:
+                    transform.position = diePositions3[beneathSurfIdx].position;
+                    break;
+                case 2:
+                    transform.position = diePositions4[beneathSurfIdx].position;
+                    break;
+                case 3:
+                    transform.position = diePositions5[beneathSurfIdx].position;
+                    break;
+
+
+            }
+
+        }
+
+        if (dead)
+            resetGame();
+
+        frameCnt++;
+
+
+        checkPoints();
+
+    }
+
+    private void resetGame()
+    {
+        currentPosition = 0;
+        transform.position = positions[currentPosition].position;
+        dead = false;
+        isDying = false;
+        divingTurtlePosition = -1;
+        beneathSurfIdx = 0;
+
+    }
+
+    private void checkPoints()
+    {
+
+        if (currentPosition == 4)
+        {
+            Debug.Log(currentPosition);
+            pointsController.SetPoint(1);
+        }
+
+    }
+
+
+   
+}
---
+++ Assets/Scripts/TurtleController.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleController : MonoBehaviour
+{
+
+
+    //public List<Transform> positions = new List<Transform>();
+    public Transform positions;
+    private const int MAX_POS_INDEX = 3;
+    int incr_pos;
+    bool dive_fullfilled = false;
+    private int mod = 20;
+    private int modSleep = 600;
+    private int frameCounter;
+    private const int SOUTH = 1;
+    private const int NORTH = -1;
+    private int dir = SOUTH;
+    int randomDiveNmb;
+    bool isDiving = false;
+
+    public bool turtleSleep = false;
+    private const int TURTLE_SLEEP_START = 300;
+    public int turtleSleepVal = 40; // TURTLE_SLEEP_START;
+
+    private int beneath_surface_idx;
+
+
+    /**
+        Sköldpaddan sover - dvs de kan inte för tillfälligt dyka. Då funktionen returnerar false kan den börja dyka närsom.
+    **/
+    private bool turtle_sleep()
+    {
+
+        turtleSleepVal--;
+        if (turtleSleepVal <= 0)
+        {
+            turtleSleep = false;
+            turtleSleepVal = TURTLE_SLEEP_START;
+            transform.GetComponent<SpriteRenderer>().color = Color.yellow;
+            transform.rotation = Quaternion.Euler(0, 0, 45);
+            return turtleSleep;
+         
+
+        } else
+        {
+            transform.GetComponent<SpriteRenderer>().color = Color.white;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+            
+
+        return true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        transform.position = positions.GetChild(0).transform.position;
+        turtleSleep = true; // börja med att sova dvs att sköldpaddorna väntar med att dyka
+        turtle_sleep();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        randomDiveNmb = Random.Range(1, 200);
+        if (randomDiveNmb >= 199 && !isDiving && !turtleSleep)
+        {
+            
+            isDiving = true;
+        }
+
+        if (isDiving)
+        {
+
+            isDiving = turtleDive();
+
+            if (!isDiving)
+            {
+                turtleSleep = true;
+               
+            }
+
+        }
+
+        if (turtleSleep)
+            turtle_sleep();
+
+
+    }
+
+    IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(20);
+        print(Time.time);
+    }
+
+
+    private bool turtleDive()
+    {
+
+
+        if (dir == SOUTH && beneath_surface_idx == 0)
+        {
+
+            //positions.GetChild(incr_pos).transform.Rotate(new Vector3(0, 0, 1));
+            //if (Mathf.Round(transform.eulerAngles.z) == 180) { transform.Rotate(new Vector3(0, 0, -180)); }
+            //positions.GetChild(incr_pos).gameObje = Color.yellow; ;
+            //transform.GetComponent<SpriteRenderer>().color = Color.yellow;
+
+
+        } else if (dir == SOUTH && beneath_surface_idx == 2)
+        {
+            //transform.GetComponent<SpriteRenderer>().color = Color.white;
+
+        }
+
+
+        //Debug.Log("" + beneath_surface_idx);
+
+        if (frameCounter % mod == 0)
+        {
+            beneath_surface_idx += dir;
+        }
+        transform.position = positions.GetChild(beneath_surface_idx).transform.position;
+
+        if (beneath_surface_idx >= MAX_POS_INDEX)
+        {
+
+          
+            dir = NORTH;
+        }
+        else if (beneath_surface_idx <= 0)
+        {
+            dir = SOUTH;
+            return false;
+        }
+
+
+        frameCounter++;
+        return true;
+
+
+    }
+
+    public int getBeneathSurfaceIdx()
+    {
+
+        return beneath_surface_idx;
+    }
+
+
+
+
+
+}
